138b52636906726bf80f0883ef3861c3d7fd6bdf
raphael.morris <rphl.morris@gmail.com>
2023-02-13 08:14:55 +0100

    init

---
+++ 06-animations/src/script.js
@@ -0,0 +1,54 @@
+import * as THREE from 'three'
+import gsap from 'gsap'
+
+console.log(gsap)
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
+
+
+// Animation
+const clock = new THREE.Clock()
+const tick = () =>
+{
+    //Time
+    const elapsedTime = clock.getElapsedTime()
+    // Update object
+    camera.rotation.x = Math.cos(elapsedTime)
+    camera.rotation.y = Math.sin(elapsedTime) 
+    camera.lookAt(mesh.position)
+
+    //Render
+    renderer.render(scene, camera)
+    
+    // Repeat
+
+    window.requestAnimationFrame(tick)
+}
+//tick()
